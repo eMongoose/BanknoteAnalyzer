16fe18977bc86220e513ce831544fb41e1ee3c18
Emily <eha80@sfu.ca>
2025-07-18 16:50:56 -0700

    added pseudocode to match the requirements

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ import sys
 def imageAnalysis(model_type, image):
   print("your choice of model: ", model_type)
   print("image: ", image)
+  
+  
+  # pass image path to wavelet transform function
+  
+  # save the processed data from the wavelet transform function
+  
+  # pass the processed data through the selected model
+  
+  # save result
+  
+  # return classification result to the user (real or fake) with a confidence %
 
+
+# joe
 if __name__ == "__main__":
-  model_type = sys.argv[1]
+  model_type = sys.argv[1] 
   image = sys.argv[2]
 
   imageAnalysis(model_type, image)
