95f76061f23b1cd056e6ee34b5931375066a9af1
kirjh <71944013+kirjh@users.noreply.github.com>
2025-07-19 14:12:07 -0700

    Delete old code

--- Python/LCmodel.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import SGDClassifier
-from picklemodel import load_model
-
-def lc_predict(X):
-  model = load_model('lc')
-  prediction = model.predict(X)
-
-  return prediction
