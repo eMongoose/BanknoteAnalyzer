d986da42f9733f094ef77c00fa93a0aa796ab016
Emily <eha80@sfu.ca>
2025-07-18 19:51:24 -0700

    moved models inside python folder
    function only supports hard coded values :)

--- Python/picklemodel.py
+++ Python/picklemodel.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 
+# store model object with the model name
 def store_model(model_name, model):
   file_path = os.path.join('Models', f'{model_name}.mdl')
   file = open(file_path, 'wb')
