3987b99fab53d5c2c69f765c369185b423d762f3
kirjh <71944013+kirjh@users.noreply.github.com>
2025-07-18 17:22:24 -0700

    Create helper function for loading and storing models

---
+++ Python/picklemodel.py
@@ -0,0 +1,15 @@
+import pickle
+import os
+
+def store_model(model_name, model):
+  file_path = os.path.join('Models', f'{model_name}.mdl')
+  file = open(file_path, 'wb')
+  pickle.dump(model, file)
+  file.close()
+
+def load_model(model_name):
+  file_path = os.path.join('Models', f'{model_name}.mdl')
+  file = open(file_path, 'rb')
+  loaded_model = pickle.load(file)
+  file.close()
+  return loaded_model
