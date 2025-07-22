7ef207a2e84a76d0b70002e7f39ae8155eafc2b1
kirjh <71944013+kirjh@users.noreply.github.com>
2025-07-21 22:50:03 -0700

    Add debug feature to run model with subset of dataset

--- Python/CNN_training.py
+++ Python/CNN_training.py
@@ -4,10 +4,11 @@ from torch.optim import SGD
 import torch.nn as nn
 from CNN import CNN
 
-def train_CNN(epochs = 20, learning_rate = 0.001):
+def train_CNN(epochs = 20, learning_rate = 0.001, debugging = False):
   local_device = device('cuda' if cuda.is_available() else 'cpu')
 
-  train_loader, test_loader = load_data(62)
+  train_loader, test_loader = load_data(64, debugging=debugging)
+  print("Data loaded")
 
   model = CNN(3)
   loss_function = nn.CrossEntropyLoss()
@@ -15,16 +16,21 @@ def train_CNN(epochs = 20, learning_rate = 0.001):
   optimizer = SGD(model.parameters(), lr=learning_rate, weight_decay = 0.005, momentum = 0.9)
   total_step = len(train_loader)
 
+  print("Beginning training")
   for epoch in range(epochs):
     for i, (images, labels) in enumerate(train_loader):
       images = images.to(local_device)
       labels = labels.to(local_device)
 
+      # forward pass
       outputs = model(images)
       loss = loss_function(outputs, labels)
 
+      # backward pass
       optimizer.zero_grad()
       loss.backward()
       optimizer.step()
     
     print(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item()}')
+
+train_CNN(debugging = True)
--- Python/dataloader.py
+++ Python/dataloader.py
@@ -2,7 +2,8 @@ import os
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, random_split, Subset
+from torch import randperm
 
 from USCoinsDataset import USCoinsDataset
 
@@ -15,19 +16,27 @@ transform = transforms.Compose([
   transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))
 ])
 
-def load_data(batch_size = 32, num_workers = 0):
+def load_data(batch_size = 32, num_workers = 0, debugging = False):
   '''
   Arguments:
     batch_size (Integer): size of each batch of images
     num_workers (Integer): number of concurrentprocesses to run
+    debugging (Bool): reduce dataset training time to test other functionalities
   Returns:
     tuple (Dataloader): train_set and test_set
   '''
   csv_file = os.path.join("Dataset", "dataset.csv")
   img_dir = os.path.join("Dataset", "coins")
   dataset = USCoinsDataset(csv_file, img_dir, transform)
+  
+  if debugging == True:
+    idx = randperm(len(dataset))[:150]
+    print(idx.numpy())
+    dataset = Subset(dataset, idx.numpy())
 
-  train_set, test_set = random_split(dataset, [3000, 749]) # roughly 4:1 split
+    train_set, test_set = random_split(dataset, [120, 30]) # 4:1 split
+  else:
+    train_set, test_set = random_split(dataset, [3000, 749]) # roughly 4:1 split
 
   train_set = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
   test_set = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
