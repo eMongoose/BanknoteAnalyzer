5c9d93a63012bd1f953b8152cda0c47cede48b44
kirjh <71944013+kirjh@users.noreply.github.com>
2025-07-24 11:55:27 -0700

    Allow POST requests to server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from markupsafe import escape
 from python.cnn.CNN_predict import predict
 
@@ -7,7 +7,11 @@ values = [0.05, 0.01, 0.25]
 
 app = Flask(__name__)
 
-@app.route("/img/<path:img_data>")
+@app.route("/")
+def index():
+  return "Server Active"
+
+@app.route("/img/<path:img_data>", methods=['GET', 'POST'])
 def img(img_data):
   label = predict(img_data)
   data = {
