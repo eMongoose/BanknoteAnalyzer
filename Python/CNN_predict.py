8e766ce6fc3bd8f0efda0b4fe7d17aa606bc4859
kirjh <71944013+kirjh@users.noreply.github.com>
2025-07-23 23:46:26 -0700

    Add model prediction on new images

--- Python/CNN_predict.py
+++ Python/CNN_predict.py
@@ -1,32 +1,29 @@
 import torch
 import os
 from CNN import CNN
+from PIL import Image
 
 from dataloader import transform, load_data
 
-def predict(img):
+def predict(img_path):
+  local_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+  # Load model from memory
   model = CNN(3)
   model.load_state_dict(torch.load(os.path.join("Python", "Models", "CNN.torch")))
 
-  local_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  # Image needs to be reshaped to include batch info (1)
+  # This is because model expects multiple images to be loaded
+  image = Image.open(img_path)
+  image = transform(image).reshape(-1, 3, 32, 32)
+  image = image.to(local_device)
 
-  train_loader, test_loader = load_data(64, debugging=True)
-
+  # Predict on image
   with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(local_device)
-        labels = labels.to(local_device)
-        outputs = model(images)
-        print(outputs.data)
-        print(outputs)
-        _, predicted = torch.max(outputs.data, 1)
-        print(_)
-        print(predicted)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-    
-    print(f'Accuracy of the network on the train images: {100 * correct / total} %')
+    model.eval()
 
-predict("a")
+    output = model(image)
+ 
+    prediction = torch.argmax(output.data).numpy()
+
+    return prediction
