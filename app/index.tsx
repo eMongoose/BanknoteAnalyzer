ea1cae195641d2fb04850b12b35e4fd99f6a9f43
Emily <eha80@sfu.ca>
2025-08-06 10:22:11 -0700

    revert

--- app/index.tsx
+++ app/index.tsx
@@ -92,22 +92,16 @@ export default function HomeScreen() {
 
   const analyzeCoin = async () => {
     console.log("Analyzing the image...");
-
-    // error  handling
     if (!img) {
       console.log("Error 1: There is no image selected!")
       return;
     }
-
-
     // https://developer.mozilla.org/en-US/docs/Web/API/FormData
     // build a formdata object
     const form = new FormData();
     const fileName = img.split('/').pop() || `coin_${Date.now()}.jpg`; // clean up the path to get the filename
     const ext = fileName.split('.').pop()?.toLowerCase(); // cleanup the filename to get the MIME extension
-
-    // file of MIME type (image, jpeg, etc.)
-    const mime = ext === 'png'
+    const mime = ext === 'png' // file of MIME type (image, jpeg, etc.)
       ? 'image/png'
       : ext === 'heic'
         ? 'image/heic'
@@ -118,7 +112,6 @@ export default function HomeScreen() {
     if (Platform.OS === 'web') {
       const imageResponse = await fetch(img);
       const blob = await imageResponse.blob();
-
       form.append('image', new File([blob], fileName, { type: mime }));
     }
     else {
@@ -128,228 +121,222 @@ export default function HomeScreen() {
         type: mime,
       } as any);
     }
-
     // fetch and send formdata to the server
-
     try {
       const response = await fetch(`${API_BASE}/getCoin`, {
         method: 'POST',
         body: form,
         headers: { Accept: 'application/json', },
       });
-
-      // error handling
       if (!response.ok) {
         const err = await response.json().catch(() => ({}));
         throw new Error(err.error || `HTTP ${response.status}`);
       }
-
       const json = await response.json();
       setCoinData(json);
       setModalVisible(true);
-
     } catch (err) {
       console.error("Analyze failed:", err);
     };
-    /////////////////////////////////////////////////////////////////////////////////////////////////
-    // main function
-    return (
-      <ParallaxScrollView
-        headerBackgroundColor={{ light: '#A1CEDC', dark: '#ffffffff' }}
-        headerImage={<Image style={styles.banner} source={require('@/assets/images/banner.png')} />}>
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  // main function
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#ffffffff' }}
+      headerImage={<Image style={styles.banner} source={require('@/assets/images/banner.png')} />}>
 
-        <ThemedView style={{ height: 500 }}>
-          {/* Title: Coin Identifier*/}
-          <ThemedView style={styles.titleContainer}>
-            <ThemedText type="title">Coin Identifier</ThemedText>
+      <ThemedView style={{ height: 500 }}>
+        {/* Title: Coin Identifier*/}
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Coin Identifier</ThemedText>
+        </ThemedView>
+
+        {isVisible ?
+          <ThemedView style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+            <ThemedView style={{ display: 'flex', flexDirection: 'row', gap: 10, width: '60%', justifyContent: 'space-around', margin: 'auto' }}>
+              <Pressable style={styles.pictureButton} onPress={openCamera}>
+                <ThemedText style={styles.pictureButtonText}>Take Picture</ThemedText>
+              </Pressable>
+              <Pressable style={styles.pictureButton} onPress={openLibrary}>
+                <ThemedText style={styles.pictureButtonText}>Select a picture</ThemedText>
+              </Pressable>
+            </ThemedView>
           </ThemedView>
+          : null
+        }
 
-          {isVisible ?
-            <ThemedView style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
+        {/* Result */}
+        {img && (
+          <ThemedView style={styles.imageContainer}>
+            <Image
+              style={{ height: '100%', width: '100%' }}
+              source={{ uri: img }}
+              contentFit="contain"
+              onLoadEnd={() => {
+                toggleVisibility();
+              }}
+            />
+          </ThemedView>
+        )}
+        {/* Use this picture */}
+        {img && (
+          <ThemedView style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
+            <ThemedView style={{ margin: 'auto' }}>
+              <Pressable style={styles.retakeButtonContainer} onPress={analyzeCoin}>
+                <ThemedText style={styles.pictureButtonText}>Analyze Coin</ThemedText>
+              </Pressable>
+            </ThemedView>
+            <ThemedText style={{ textAlign: 'center', fontWeight: 500 }}>OR</ThemedText>
+            <ThemedView>
               <ThemedView style={{ display: 'flex', flexDirection: 'row', gap: 10, width: '60%', justifyContent: 'space-around', margin: 'auto' }}>
                 <Pressable style={styles.pictureButton} onPress={openCamera}>
-                  <ThemedText style={styles.pictureButtonText}>Take Picture</ThemedText>
+                  <ThemedText style={styles.pictureButtonText}>Retake Picture</ThemedText>
                 </Pressable>
                 <Pressable style={styles.pictureButton} onPress={openLibrary}>
                   <ThemedText style={styles.pictureButtonText}>Select a picture</ThemedText>
                 </Pressable>
               </ThemedView>
             </ThemedView>
-            : null
-          }
+          </ThemedView>
+        )}
 
-          {/* Result */}
-          {img && (
-            <ThemedView style={styles.imageContainer}>
-              <Image
-                style={{ height: '100%', width: '100%' }}
-                source={{ uri: img }}
-                contentFit="contain"
-                onLoadEnd={() => {
-                  toggleVisibility();
-                }}
-              />
-            </ThemedView>
-          )}
-          {/* Use this picture */}
-          {img && (
-            <ThemedView style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
-              <ThemedView style={{ margin: 'auto' }}>
-                <Pressable style={styles.retakeButtonContainer} onPress={analyzeCoin}>
-                  <ThemedText style={styles.pictureButtonText}>Analyze Coin</ThemedText>
-                </Pressable>
+        {/* Popup */}
+        <Modal
+          transparent={true}
+          visible={modalVisible}
+        >
+          <ThemedView style={styles.centeredView}>
+            <ThemedView style={styles.modalView}>
+              <ThemedText style={styles.modalTitle}>Coin Results:</ThemedText>
+
+              {/* POPUP CONTENTS HERE */}
+              <ThemedView style={styles.resultsTextContainer}>
+                <ThemedText style={styles.resultsText}>This coin is predicted to be a {coinData?.name}</ThemedText>
+                <ThemedText style={styles.resultsText}>The coin's value is {coinData?.value}</ThemedText>
               </ThemedView>
-              <ThemedText style={{ textAlign: 'center', fontWeight: 500 }}>OR</ThemedText>
-              <ThemedView>
-                <ThemedView style={{ display: 'flex', flexDirection: 'row', gap: 10, width: '60%', justifyContent: 'space-around', margin: 'auto' }}>
-                  <Pressable style={styles.pictureButton} onPress={openCamera}>
-                    <ThemedText style={styles.pictureButtonText}>Retake Picture</ThemedText>
-                  </Pressable>
-                  <Pressable style={styles.pictureButton} onPress={openLibrary}>
-                    <ThemedText style={styles.pictureButtonText}>Select a picture</ThemedText>
-                  </Pressable>
-                </ThemedView>
-              </ThemedView>
+              {/* CLOSE BUTTON */}
+              <Pressable
+                style={styles.buttonClose}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <ThemedText style={styles.textStyle}>Close</ThemedText>
+              </Pressable>
             </ThemedView>
-          )}
+          </ThemedView>
+        </Modal>
 
-          {/* Popup */}
-          <Modal
-            transparent={true}
-            visible={modalVisible}
-          >
-            <ThemedView style={styles.centeredView}>
-              <ThemedView style={styles.modalView}>
-                <ThemedText style={styles.modalTitle}>Coin Results:</ThemedText>
+        {/* END OF TAG */}
+      </ThemedView>
+    </ParallaxScrollView >
+  );
+}
 
-                {/* POPUP CONTENTS HERE */}
-                <ThemedView style={styles.resultsTextContainer}>
-                  <ThemedText style={styles.resultsText}>This coin is predicted to be a {coinData?.name}</ThemedText>
-                  <ThemedText style={styles.resultsText}>The coin's value is {coinData?.value}</ThemedText>
-                </ThemedView>
-                {/* CLOSE BUTTON */}
-                <Pressable
-                  style={styles.buttonClose}
-                  onPress={() => setModalVisible(!modalVisible)}>
-                  <ThemedText style={styles.textStyle}>Close</ThemedText>
-                </Pressable>
-              </ThemedView>
-            </ThemedView>
-          </Modal>
+const styles = StyleSheet.create({
+  banner: {
+    height: '100%',
+    width: '100%',
+  },
 
-          {/* END OF TAG */}
-        </ThemedView>
-      </ParallaxScrollView >
-    );
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  pictureButton: {
+    borderWidth: 1.5,
+    borderRadius: 20,
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    minWidth: 150,
+    backgroundColor: 'white',
+    cursor: 'pointer',
+  },
+
+  pictureButtonText: {
+    color: 'black',
+    fontWeight: 700,
+  },
+
+  imageContainer: {
+    height: 300,
+    width: 300,
+    margin: 'auto',
+  },
+
+  retakeButtonContainer: {
+    borderWidth: 1.5,
+    borderRadius: 20,
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: 200,
+    backgroundColor: 'white',
+    cursor: 'pointer',
+  },
+
+  /*****************************************************/
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.80)',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    gap: 25,
+    paddingLeft: 100,
+    paddingRight: 100
+  },
+
+  buttonClose: {
+    borderRadius: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+    elevation: 2,
+    backgroundColor: '#2196F3',
+  },
+
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  modalTitle: {
+    marginBottom: 15,
+    textAlign: 'center',
+    color: 'black'
+  },
+
+  /************************************/
+  resultsTextContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    backgroundColor: 'white'
+  },
+
+  resultsText: {
+    textAlign: 'center',
+    color: 'black'
   }
-
-  const styles = StyleSheet.create({
-    banner: {
-      height: '100%',
-      width: '100%',
-    },
-
-    titleContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-
-    pictureButton: {
-      borderWidth: 1.5,
-      borderRadius: 20,
-      alignItems: 'center',
-      paddingLeft: 20,
-      paddingRight: 20,
-      paddingTop: 10,
-      paddingBottom: 10,
-      minWidth: 150,
-      backgroundColor: 'white',
-      cursor: 'pointer',
-    },
-
-    pictureButtonText: {
-      color: 'black',
-      fontWeight: 700,
-    },
-
-    imageContainer: {
-      height: 300,
-      width: 300,
-      margin: 'auto',
-    },
-
-    retakeButtonContainer: {
-      borderWidth: 1.5,
-      borderRadius: 20,
-      alignItems: 'center',
-      paddingLeft: 20,
-      paddingRight: 20,
-      paddingTop: 10,
-      paddingBottom: 10,
-      width: 200,
-      backgroundColor: 'white',
-      cursor: 'pointer',
-    },
-
-    /*****************************************************/
-    centeredView: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.80)',
-    },
-    modalView: {
-      margin: 20,
-      backgroundColor: 'white',
-      borderRadius: 20,
-      padding: 35,
-      alignItems: 'center',
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5,
-      gap: 25,
-      paddingLeft: 100,
-      paddingRight: 100
-    },
-
-    buttonClose: {
-      borderRadius: 20,
-      paddingLeft: 20,
-      paddingRight: 20,
-      paddingTop: 5,
-      paddingBottom: 5,
-      elevation: 2,
-      backgroundColor: '#2196F3',
-    },
-
-    textStyle: {
-      color: 'white',
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-
-    modalTitle: {
-      marginBottom: 15,
-      textAlign: 'center',
-      color: 'black'
-    },
-
-    /************************************/
-    resultsTextContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      gap: 10,
-      backgroundColor: 'white'
-    },
-
-    resultsText: {
-      textAlign: 'center',
-      color: 'black'
-    }
-  })
-};
+});
